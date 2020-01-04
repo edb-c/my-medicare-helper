f46a1d4f9f7685adaf479072295208536658d9b7
edb-c <edb.cofresi@gmail.com>
2020-01-04 12:06:46 -0500

    import design-system-core css

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,21 @@
-/* import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Import all components and reference one by name. You can optimize this using
+// your build process (ie. Tree shaking in Webpack)
+//import { Alert } from "@cmsgov/design-system-core";
+// Import individual component. No special optimizations needed.
+import Button from "@cmsgov/design-system-core/dist/components/Button/Button";
+import React from "react";
+import "@cmsgov/design-system-core/dist/index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="ds-base ds-u-padding--2">
+    
+        <p className="ds-c-alert__text">
+          You did it! You&rsquo;ve ran the example.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button className="ds-u-margin-top--2">Learn more</Button>
+
     </div>
   );
 }
-
-export default App;
-*/
-// Import all components and reference one by name. You can optimize this using
-// your build process (ie. Tree shaking in Webpack)
-import { Alert } from '@cmsgov/design-system-core';
-// Import individual component. No special optimizations needed.
-import Button from '@cmsgov/design-system-core/dist/components/Button/Button';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-function App() {
-  return (
-    <div>
-      <Alert heading="Hello world">
-        <p className="ds-c-alert__text">You did it! You&rsquo;ve ran the example.</p>
-        <Button className="ds-u-margin-top--2">Learn more</Button>
-      </Alert>
-    </div>
-  );
-  } 
 export default App;
